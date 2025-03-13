ce8c6bb2526d32dd0cd440497ae50cea65cd6705
antazoey <antyzoa@gmail.com>
2025-03-13 12:12:34 -0500

    feat: updates

--- ape_keyring/__init__.py
+++ ape_keyring/__init__.py
@@ -2,23 +2,32 @@ from pathlib import Path
 
 import ape.plugins
 
-from ._secrets import Scope, get_secret_manager
-from .accounts import KeyringAccount, KeyringAccountContainer
-from .config import KeyringConfig
+import ape_keyring._secrets
 
 
 @ape.plugins.register(ape.plugins.Config)
 def config_class():
+    from .config import KeyringConfig
+
     return KeyringConfig
 
 
 @ape.plugins.register(ape.plugins.AccountPlugin)
 def account_types():
+    from .accounts import KeyringAccount, KeyringAccountContainer
+
     return KeyringAccountContainer, KeyringAccount
 
 
+def __getattr__(name):
+    import ape_keyring._secrets
+
+    return getattr(ape_keyring._secrets, name)
+
+
+# TODO: Figure out how NOT to do this at plugin load time.
 # Sync environment variables if configured to do so.
-secret_manager = get_secret_manager(Path.cwd())
+secret_manager = ape_keyring._secrets.get_secret_manager(Path.cwd())
 secret_manager.set_environment_variables()
 
 __all__ = ["Scope", "secret_manager"]
--- tests/conftest.py
+++ tests/conftest.py
@@ -16,12 +16,7 @@ def config():
 
 
 @pytest.fixture(scope="session", autouse=True)
-def project():
-    return ape.project
-
-
-@pytest.fixture(scope="session", autouse=True)
-def backend(accounts):
+def backend():
     backend = EphemeralBackend()
     keyring.set_keyring(backend)
     return backend
@@ -38,15 +33,10 @@ def secret_manager(container, storage, project):
 
 
 @pytest.fixture(scope="session", autouse=True)
-def container(accounts, storage):
-    container = accounts.containers["keyring"]
+def container(storage):
+    container = ape.accounts.containers["keyring"]
     container.storage = storage
     return container
-
-
-@pytest.fixture(scope="session")
-def accounts():
-    return ape.accounts
 
 
 @pytest.fixture
@@ -71,7 +61,7 @@ def non_existing_alias():
 
 @pytest.fixture
 def test_account(accounts):
-    return accounts.test_accounts[0]
+    return accounts[0]
 
 
 @pytest.fixture
